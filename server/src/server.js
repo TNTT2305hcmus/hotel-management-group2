4bf73da93e4b26f04c17f84fb67be4de746dbc0c
minh thang <minhthang@gmail.com>
2025-12-14 11:06:14 +0700

    feat(auth): add login and register without database

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.js'
 const app = express()
 const PORT = process.env.PORT || 8888
+//Middleware
+app.use(express.json())
+
+
+app.get('/',(req,res) => {
+  res.sendStatus(200)
+})
+
+
+//Routes
+app.use('/auth',authRoutes)
 
 
 app.listen(PORT , ()=> console.log(`server has started on PORT: ${PORT} `))
