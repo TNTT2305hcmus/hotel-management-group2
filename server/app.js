e72ed8cb34c9cb9bf5ec54c1e9a86af98147a026
minh thang <minhthang@gmail.com>
2025-12-14 10:11:09 +0700

    chore: change init backend

--- server/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express(); 
-
-app.listen(8080, () => {
-    console.log("Server đang chạy tại port 8080");
-});
---
+++ server/src/server.js
@@ -0,0 +1,6 @@
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 8888
+
+
+app.listen(PORT , ()=> console.log(`server has started on PORT: ${PORT} `))
